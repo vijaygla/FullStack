b8e5bac406f6865a0bc067acd64ebcb9c5154af6
Vijay Kumar <vijaykumarmganj1@gmail.com>
2024-08-13 12:45:51 +0530

    Today we start Js

---
+++ JAVA_SCRIPT/index.js
@@ -0,0 +1,21 @@
+const amount = 20;
+console.log("🚀 ~ amount:", amount);
+
+
+const ans = true;
+console.log("🚀 ~ ans:", ans)
+
+
+
+const userName  =  [
+    "user101",
+    "user102",
+    "user103",
+    "user104",
+    "user105",
+    {"key1" : "Value1"},
+];
+
+console.log(userName[5]);
+console.log(userName[5].key1);
+
